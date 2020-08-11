5437cef337c157fa87bfc7470ce1cec05f3a06e3
mileshkopetr <mileshko.petr@yandex.ru>
2020-08-11 16:25:46 +0300

    DbInit.php modified
    Предлагаю такой способ синглтона БД

--- models/DbInit.php
+++ models/DbInit.php
@@ -9,19 +9,24 @@ namespace School\models;
 final class DbInit {
     
     private static $connection;
-    private $params;
+
+    /*
+
+    Пример  инициализации :
+
+    $connect = School\models\DbInit::prepare('Select * From Table');
+
+    */
+  
     
-    public function __construct () {
-        
-        $this->params = \Init::getDbParams();
-    }
-    
-   public static function getConnection () {
+   final private static function getConnection () {
         
         if(self::$connection){
           return self::$connection;
         }
        
+       $params = \Init::getDBParams();
+
        self::$connection = new \PDO($params['dsn'], $params['user'], $params['password']);
        self::$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
        
@@ -36,6 +41,11 @@ final class DbInit {
         
     } 
     
+    public static function __callStatic ( $name, $args ) {
+        $callback =  [self :: getConnection ( ), $name ] ;
+        return call_user_func_array ( $callback , $args ) ;
+    } 
+
 
 }
 
