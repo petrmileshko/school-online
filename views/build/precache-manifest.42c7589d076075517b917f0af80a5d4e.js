94d252d9e1cdf8f1e21ae383470ce9b651459ea5
Vitaliy Chalin <vitaly.chalin@gmail.com>
2020-08-13 20:53:07 +0500

    Добавлена папка build

---
+++ views/build/precache-manifest.42c7589d076075517b917f0af80a5d4e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71053c6ef1b76ac0042ad1d5f4e48ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb169cd1953ac9c1a1a2",
+    "url": "/static/css/main.d76b19df.chunk.css"
+  },
+  {
+    "revision": "20e8adbab75b999b90cd",
+    "url": "/static/js/2.916fdd22.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.916fdd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb169cd1953ac9c1a1a2",
+    "url": "/static/js/main.680043d7.chunk.js"
+  },
+  {
+    "revision": "3916f632185066875933",
+    "url": "/static/js/runtime-main.97c3208e.js"
+  },
+  {
+    "revision": "131b7f1e91a652791f08f5ccfe702645",
+    "url": "/static/media/line-awesome.131b7f1e.svg"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "21aa374dd67a616b26e6ebccc72c6d20",
+    "url": "/static/media/logo.21aa374d.png"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/static/media/logout.e75855a1.svg"
+  },
+  {
+    "revision": "c9e00efdaecda986bc48678a860863f5",
+    "url": "/static/media/open-book.c9e00efd.svg"
+  },
+  {
+    "revision": "5e946c20186e33b608fbb3388fff6ff5",
+    "url": "/static/media/user.5e946c20.svg"
+  }
+]);
