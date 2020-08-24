4b54de2468143635325e7a3e0d2f4e19a031270e
Vitaliy Chalin <vitaly.chalin@gmail.com>
2020-08-24 17:52:20 +0500

    fix

--- views/src/context/AuthContext.js
+++ views/src/context/AuthContext.js
@@ -5,10 +5,6 @@ function noop() {}
 export const AuthContext = createContext({
     token: null,
     userId: null,
-    userName: null,
-    userEmail: null,
-    userAccess: null,
-    userSubject: null,
     login: noop,
     logout: noop,
     isAuthenticated: false
