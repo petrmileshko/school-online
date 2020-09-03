85a01f2c1c2b1f8158da652766250d2a3ff2bf69
mileshkopetr <mileshko.petr@yandex.ru>
2020-09-04 01:37:50 +0300

    доюавил код Александра
    файл Answers.php

--- controllers/Answers.php
+++ controllers/Answers.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-        code by  
+        code by  Aleksandr Baukov
         Контроллер для работы с моделью - БД / талица Answers
 
 */
@@ -10,25 +10,84 @@ namespace School\controllers;
 
 
 
+use School\models\SQL;
+
 class Answers extends Controller {
 
+    private $id;
+    //private $array;
 
     use TraitControllers;
     
     public function __construct ($rest) {
         parent::__construct ($rest);
+        $this->id = parent::getValue('id');
     }
-    
-        public function action_scores() {
-
-            # Start...    тестовый код - после отладки заменить на рабочий
-
-                     return json_encode($this->answers);
 
 
-           # End  ...    тестовый код
-        } 
-        
+    /**
+     * @return false|string
+     * @throws \Exception
+     */
+    public function action_getAnswer() {
+
+        $answer = $this->getValue( null, $this->id, $this->query['action']);
+
+        if( $answer ) {
+            return json_encode($answer);
+        }
+
+        $message = 'Ответ не найден : '.$this->id;
+
+        $this->fail($message);
+
+    }
+
+
+    /**
+     * @return false|string
+     * @throws \Exception
+     */
+    public function action_getAnswers() {
+
+        $answers = $this->getValue( null, null , $this->query['action']);
+
+        if( $answers ) {
+            return json_encode($answers);
+        }
+
+        $this->fail('Ответов в базе нет.');
+    }
+
+
+    /**
+     * @return false|string
+     * @throws \Exception
+     */
+    public function action_Update() {
+
+        $result =  $this->setValue( 'id='.$this->id, array_slice($this->query, 3) , $this->query['action']);
+
+        if( $result ) {
+            return json_encode(['result'=>'positive', 'message'=>$result]);
+        }
+
+        $this->fail('Ошибка обновления данных в таблице: '.$this->controller);
+    }
+
+    /**
+     * @return false|string
+     * @throws \Exception
+     */
+    public function action_Insert(){
+        $result =  $this->setValue(null, array_slice($this->query, 3) , $this->query['action']);
+
+        if( $result ) {
+            return json_encode(['result'=>'positive', 'message'=>$result]);
+        }
+
+        $this->fail('Ошибка при добавлении данных в таблицу: '.$this->controller);
+    }
 
 }
 
