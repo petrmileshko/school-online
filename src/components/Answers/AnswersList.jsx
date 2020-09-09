51c252e8da8757f68e82af07c53ceead65cf7861
Vitaliy Chalin <vitaly.chalin@gmail.com>
2020-09-09 11:28:15 +0500

    Add Answers Get

--- src/components/Answers/AnswersList.jsx
+++ src/components/Answers/AnswersList.jsx
@@ -7,79 +7,27 @@ import Spinner from 'react-bootstrap/Spinner';
 export const AnswersList = () => {
 	const { loading, request } = useHttp();
 	const [answers, setAnswers] = useState([]);
-	const [subjectsList, setSubjectsList] = useState([]);
 	const getAnswersData = useCallback(
 		async (signal) => {
 			try {
 				const data = await request(
 					'https://cors-anywhere.herokuapp.com/http://test-school.webpeternet.com/MainController.php',
 					'POST',
-					{ Table: 'Answers', action: 'scores', signal: signal },
+					{
+						Table: 'Answers',
+						action: 'getAnswers',
+						question: 'string',
+						signal: signal,
+					},
 				);
 
-				handleData(data);
-				getAllSubjects(data);
+				setAnswers(data);
 			} catch (err) {
 				console.log(('___POST_ANSWERS___', err));
 			}
 		},
 		[request],
 	);
-
-	const handleData = (data) => {
-		const vocabulary = [];
-		data.forEach((dataItem) => {
-			if (dataItem.score !== null) {
-				if (vocabulary.length > 0) {
-					const student = vocabulary.find((el) => el.name === dataItem.fio);
-					if (student) {
-						const subj = student.subjects.find(
-							(s) => s.title === dataItem.subject,
-						);
-						if (subj) {
-							subj.scores.push(dataItem.score);
-						} else {
-							student.subjects.push({
-								title: dataItem.subject,
-								scores: [dataItem.score],
-							});
-						}
-					} else {
-						vocabulary.push({
-							name: dataItem.fio,
-							subjects: [{ title: dataItem.subject, scores: [dataItem.score] }],
-						});
-					}
-				} else {
-					vocabulary.push({
-						name: dataItem.fio,
-						subjects: [{ title: dataItem.subject, scores: [dataItem.score] }],
-					});
-				}
-			}
-		});
-
-		vocabulary.forEach((stud) => {
-			stud.subjects.forEach((subj) => {
-				subj.scores = average(subj.scores);
-			});
-		});
-		setAnswers(vocabulary);
-	};
-
-	const getAllSubjects = (data) => {
-		let result = [];
-		data.forEach((el) => {
-			result.push(el.subject);
-		});
-
-		result = result.filter((item, pos) => result.indexOf(item) === pos);
-		setSubjectsList(result);
-	};
-
-	const average = (nums) => {
-		return nums.reduce((a, b) => a + b) / nums.length;
-	};
 
 	useEffect(() => {
 		const abortController = new AbortController();
@@ -105,17 +53,15 @@ export const AnswersList = () => {
 						<table className="table table-hover mb-0">
 							<thead>
 								<tr>
+									<th>Дата</th>
 									<th>ФИО</th>
-									{subjectsList.map((subjName, i) => (
-										<th key={i}>{subjName}</th>
-									))}
+									<th>Задание</th>
+									<th>Оценка</th>
+									<th>Действия</th>
 								</tr>
 							</thead>
 							<tbody>
-								<AnswersListItem
-									answers={answers}
-									subjectsList={subjectsList}
-								/>
+								<AnswersListItem answers={answers} />
 							</tbody>
 						</table>
 					</div>
--- src/components/Answers/AnswersListItem.jsx
+++ src/components/Answers/AnswersListItem.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const AnswersListItem = ({ answers, subjectsList }) => {
-  const answerItem = answers.map((el, i) => {
-    return (
-      <tr key={i}>
-        <td>{el.name}</td>
-        {subjectsList.map((subj, i) => {
-          const score = el.subjects.find((el) => el.title === subj).scores;
-          return <td key={i}>{score}</td>;
-        })}
-      </tr>
-    );
-  });
+export const AnswersListItem = ({ answers }) => {
+	answers.sort((a, b) => {
+		if (a.score > 0) {
+			return 1;
+		}
+		if (a.score === '0') {
+			return -1;
+		}
 
-  return answerItem;
+		return 0;
+	});
+
+	const answerItem = answers.map((el, i) => {
+		return (
+			<tr key={i}>
+				<td>{new Date(el.time_stamp).toLocaleDateString()}</td>
+				<td>{el.fio}</td>
+				<td>
+					<Link to={`/answer/${el.id}`} title="Проверить ответ на задание">
+						{el.task_name}
+					</Link>
+				</td>
+				<td className="text-center">{el.score > 0 && el.score}</td>
+				<td className="text-center td-actions">
+					{el.score > 0 && (
+						<i className="la la-check-circle" title="Проверено"></i>
+					)}
+					{el.score <= 0 && (
+						<Link to={`/answer/${el.id}`} title="Проверить ответ">
+							<i className="la la-edit edit"></i>
+						</Link>
+					)}
+				</td>
+			</tr>
+		);
+	});
+
+	return answerItem;
 };
